ab075308484c4683d5f22dc71927ff26509f12d2
mortex1111 <128226208+mortex1111@users.noreply.github.com>
2026-04-11 23:09:13 +0200

    map

--- Enemies/screamer/screamer_circle.gd
+++ Enemies/screamer/screamer_circle.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
 func _ready() -> void:
-	var scale_anim = get_tree().create_tween().tween_property(self,"scale",Vector2(4,4),8.0) 
+	var scale_anim = get_tree().create_tween().tween_property(self,"scale",Vector2(6,6),12.0) 
 	await scale_anim.finished 
 	queue_free()
